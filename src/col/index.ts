c64d3ea8751b266840c1925d2ff310500f2bcc9f
fanhaoyuan <418536538@qq.com>
2021-09-12 00:23:01 +0800

    types: add `Col` props

---
+++ src/col/index.ts
@@ -0,0 +1,11 @@
+export interface VantColProps {
+    /**
+     * 列元素宽度
+     */
+    span?: string | number;
+
+    /**
+     * 列元素偏移距离
+     */
+    offset?: string | number;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './button';
 export * from './cell';
 export * from './cell-group';
+export * from './col';
 export * from './config-provider';
 export * from './icon';
 export * from './image';
