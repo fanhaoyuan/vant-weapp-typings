b0b4d178eec4e55955126287a104f678380ba0fa
fanhaoyuan <418536538@qq.com>
2021-09-14 17:26:02 +0800

    refactor: `GoodsAction` extends `OpenType`

--- src/goods-action-button/index.ts
+++ src/goods-action-button/index.ts
@@ -1,4 +1,6 @@
-export interface VantGoodsActionButtonProps {
+import { VantOpenTypeProps } from '../base/open-type';
+
+export interface VantGoodsActionButtonProps extends Omit<VantOpenTypeProps, 'business-id'> {
     /**
      * 按钮文字
      */
@@ -44,57 +46,6 @@ export interface VantGoodsActionButtonProps {
      * 微信开放能力，具体支持可参考 微信官方文档
      */
     'open-type'?: string;
-
-    /**
-     * 打开 `APP` 时，向 `APP` 传递的参数
-     */
-    'app-parameter'?: string;
-
-    /**
-     * 指定返回用户信息的语言
-     *
-     * `zh_CN` 简体中文
-     *
-     * `zh_TW` 繁体中文
-     *
-     * `en` 英文
-     *
-     * @default 'en'
-     */
-    lang?: 'zh_CN' | 'zh_TW' | 'en';
-
-    /**
-     * 会话来源
-     */
-    'session-from'?: string;
-
-    /**
-     * 会话内消息卡片标题
-     *
-     * @default 当前标题
-     */
-    'send-message-title'?: string;
-
-    /**
-     * 会话内消息卡片点击跳转小程序路径
-     *
-     * @default 当前分享路径
-     */
-    'send-message-path'?: string;
-
-    /**
-     * sendMessageImg
-     *
-     * @default 截图
-     */
-    'send-message-img'?: string;
-
-    /**
-     * 显示会话内消息卡片
-     *
-     * @default false
-     */
-    'show-message-card'?: string;
 
     /**
      * 按钮类型
--- src/goods-action-icon/index.ts
+++ src/goods-action-icon/index.ts
@@ -1,4 +1,6 @@
-export interface VantGoodsActionIconProps {
+import { VantOpenTypeProps } from "..";
+
+export interface VantGoodsActionIconProps extends Omit<VantOpenTypeProps, 'business-id'>{
     /**
      * 按钮文字
      */
@@ -49,55 +51,4 @@ export interface VantGoodsActionIconProps {
      * 微信开放能力，具体支持可参考 微信官方文档
      */
     'open-type'?: string;
-
-    /**
-     * 打开 `APP` 时，向 `APP` 传递的参数
-     */
-    'app-parameter'?: string;
-
-    /**
-     * 指定返回用户信息的语言
-     *
-     * `zh_CN` 简体中文
-     *
-     * `zh_TW` 繁体中文
-     *
-     * `en` 英文
-     *
-     * @default 'en'
-     */
-    lang?: 'zh_CN' | 'zh_TW' | 'en';
-
-    /**
-     * 会话来源
-     */
-    'session-from'?: string;
-
-    /**
-     * 会话内消息卡片标题
-     *
-     * @default 当前标题
-     */
-    'send-message-title'?: string;
-
-    /**
-     * 会话内消息卡片点击跳转小程序路径
-     *
-     * @default 当前分享路径
-     */
-    'send-message-path'?: string;
-
-    /**
-     * sendMessageImg
-     *
-     * @default 截图
-     */
-    'send-message-img'?: string;
-
-    /**
-     * 显示会话内消息卡片
-     *
-     * @default false
-     */
-    'show-message-card'?: string;
 }
