e810cd64926a2fb928ceb270993af419e0725249
fanhaoyuan <418536538@qq.com>
2022-05-05 15:58:29 +0800

    feat(Collapse): 新增`BaseEvent`和`ChangeEvent`

--- packages/vant-weapp-typings/src/collapse/index.ts
+++ packages/vant-weapp-typings/src/collapse/index.ts
@@ -25,21 +25,37 @@ export interface VantCollapseProps {
     border?: boolean;
 }
 
+export interface VantCollapseBaseEvent extends WechatMiniprogram.BaseEvent {
+    /**
+     * 当前打开的面板 name
+     */
+    detail: string;
+}
+
+export interface VantCollapseChangeEvent extends WechatMiniprogram.BaseEvent {
+    /**
+     * 手风琴模式时，为 name
+     *
+     * 非手风琴模式时，为打开的折叠面板的 name 数组
+     */
+    detail: string | string[];
+}
+
 export interface VantCollapseEvents {
     /**
      * 切换面板时触发
      */
-    'bind:change'?: WechatMiniprogram.EventCallback;
+    'bind:change'?: (event: VantCollapseChangeEvent) => void;
 
     /**
      * 展开面板时触发
      */
-    'bind:open'?: WechatMiniprogram.EventCallback;
+    'bind:open'?: (event: VantCollapseBaseEvent) => void;
 
     /**
      * 关闭面板时触发
      */
-    'bind:close'?: WechatMiniprogram.EventCallback;
+    'bind:close'?: (event: VantCollapseBaseEvent) => void;
 }
 
 export type VantCollapseExternalClassName = VantBaseExternalClassName;
