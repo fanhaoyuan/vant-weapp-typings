b722744dc73fc1de1ad7da51a29c73d8ee7f431a
fanhaoyuan <418536538@qq.com>
2022-05-06 17:37:18 +0800

    feat(DatetimePicker): 更新并导出事件回调

--- packages/react-vant-weapp-typings/src/datetime-picker/index.ts
+++ packages/react-vant-weapp-typings/src/datetime-picker/index.ts
@@ -3,11 +3,18 @@ import {
     VantDateTimePickerEvents,
     VantDateTimePickerExternalClassName,
     VantDateTimePickerProps,
+    VantDateTimePickerBaseEvent,
 } from 'vant-weapp-typings';
 
 type DateTimePicker = React.ComponentType<VantDateTimePicker>;
 
-export { VantDateTimePicker, VantDateTimePickerEvents, VantDateTimePickerProps, VantDateTimePickerExternalClassName };
+export {
+    VantDateTimePicker,
+    VantDateTimePickerEvents,
+    VantDateTimePickerProps,
+    VantDateTimePickerExternalClassName,
+    VantDateTimePickerBaseEvent,
+};
 
 declare const DateTimePicker: DateTimePicker;
 
