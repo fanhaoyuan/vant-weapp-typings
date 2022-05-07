4ebdb82ce38ed6129b45681857e2ed2ff661fc39
fanhaoyuan <418536538@qq.com>
2022-05-07 15:15:49 +0800

    feat(Base): `react`模块导出`base`类型

---
+++ packages/react-vant-weapp-typings/src/base/index.ts
@@ -0,0 +1,31 @@
+import {
+    ComponentBase,
+    VantComponent,
+    VantEvent,
+    VantTouchEvent,
+    VantEventHandler,
+    VantTouchEventHandler,
+    TouchPositionDetail,
+    VantBaseExternalClassName,
+    VantBaseLoadingExternalClassName,
+    WechatOpenType,
+    VantOpenTypeProps,
+    VantOpenTypeCamelCaseProps,
+    VantOpenTypeEvents,
+} from 'vant-weapp-typings';
+
+export {
+    ComponentBase,
+    VantComponent,
+    VantEvent,
+    VantTouchEvent,
+    VantEventHandler,
+    VantTouchEventHandler,
+    TouchPositionDetail,
+    VantBaseExternalClassName,
+    VantBaseLoadingExternalClassName,
+    WechatOpenType,
+    VantOpenTypeProps,
+    VantOpenTypeCamelCaseProps,
+    VantOpenTypeEvents,
+};
